756d92db542fb2450eb2a05d64df125627d6e352
steveyout <34503856+steveyout@users.noreply.github.com>
2018-10-22 18:31:23 +0300

    Update migrate-database.sh

--- migrate-database.sh
+++ migrate-database.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e 
 
-echo "Migrating database 'php artisan migrate --force'..."
-php artisan migrate --force
+echo "Migrating database 'php artisan migrate '..."
+php artisan migrate 
